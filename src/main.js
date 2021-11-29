9fc06fa129ca23a170c05e629bc781658555c1c7
Макс <0w1.go.0w1@gmail.com>
2021-11-29 14:25:55 +0300

    Создал компонент, отвечающий за profile в header и отрисовал его на сайте

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import {RenderPosition, renderTemplate} from './render.js';
+import {createProfileElement} from './view/profile-view.js';
 import {createNavElement, createSortElement} from './view/menu-view.js';
 
 const headerElement = document.querySelector('.header');
 const mainElement = document.querySelector('.main');
 const footerElement = document.querySelector('.footer');
 
+renderTemplate(headerElement, createProfileElement(), RenderPosition.BEFOREEND);
 renderTemplate(mainElement, createNavElement(), RenderPosition.BEFOREEND);
 renderTemplate(mainElement, createSortElement(), RenderPosition.BEFOREEND);
