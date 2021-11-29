dbe97fd904c9ee6b2cda90e74b1333a95da7ba94
Макс <0w1.go.0w1@gmail.com>
2021-11-29 22:53:29 +0300

    Создал компонент, отвечающий за кнопку Show More и отрисовал её на сайте

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import {createProfileElement} from './view/profile-view.js';
 import {createNavElement, createSortElement} from './view/menu-view.js';
 import {createFilmsListElement} from './view/films-list-view.js';
 import {createFilmCardElement} from './view/film-card-view.js';
+import {createButtonShowMore} from './view/show-more-view.js';
 
 const headerElement = document.querySelector('.header');
 const mainElement = document.querySelector('.main');
@@ -14,7 +15,11 @@ renderTemplate(mainElement, createSortElement(), RenderPosition.BEFOREEND);
 
 const FILMS_COUNT = 5;
 renderTemplate(mainElement, createFilmsListElement(), RenderPosition.BEFOREEND);
-const filmsListElement = document.querySelector('.films-list__container');
+const filmsListContainerElement = document.querySelector('.films-list__container');
+
 for (let index = 0; index < FILMS_COUNT; index++) {
-  renderTemplate(filmsListElement, createFilmCardElement(), RenderPosition.BEFOREEND);
+  renderTemplate(filmsListContainerElement, createFilmCardElement(), RenderPosition.BEFOREEND);
 }
+
+const filmsListElement = document.querySelector('.films-list');
+renderTemplate(filmsListElement, createButtonShowMore(), RenderPosition.BEFOREEND);
