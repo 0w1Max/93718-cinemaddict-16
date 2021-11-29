dd77285f0d2d226a076210975567688f6510427d
Макс <0w1.go.0w1@gmail.com>
2021-11-29 14:14:10 +0300

    Создал компонент menu-view в папке src/view для отрисовки навигации и сортировки. В main.js добавил отрисовку компонента

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import {RenderPosition, renderTemplate} from './render.js';
+import {createNavElement, createSortElement} from './view/menu-view.js';
 
 const headerElement = document.querySelector('.header');
 const mainElement = document.querySelector('.main');
 const footerElement = document.querySelector('.footer');
+
+renderTemplate(mainElement, createNavElement(), RenderPosition.BEFOREEND);
+renderTemplate(mainElement, createSortElement(), RenderPosition.BEFOREEND);
