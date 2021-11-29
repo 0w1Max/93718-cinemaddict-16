713031dbce8ceea8bfe32947e93e83112cdb7274
Макс <0w1.go.0w1@gmail.com>
2021-11-30 01:17:22 +0300

    Создал компонент с подробной информацией о фильме (попап) и скрыл его

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import {createFilmsListElement} from './view/films-list-view.js';
 import {createFilmCardElement} from './view/film-card-view.js';
 import {createShowMoreButtonElement} from './view/show-more-buttom-view.js';
 import {createFooterStatisticsElement} from './view/footer-statistics-view.js';
+import {createFilmDetailsElement} from './view/film-details-view.js';
 
 const FILM_COUNT = 5;
 
@@ -18,13 +19,17 @@ renderTemplate(mainElement, createSortElement(), RenderPosition.BEFOREEND);
 
 renderTemplate(mainElement, createFilmsListElement(), RenderPosition.BEFOREEND);
 
-const filmsListElement = document.querySelector('.films-list');
 const filmsListContainerElement = document.querySelector('.films-list__container');
 
 for (let index = 0; index < FILM_COUNT; index++) {
   renderTemplate(filmsListContainerElement, createFilmCardElement(), RenderPosition.BEFOREEND);
 }
 
-renderTemplate(filmsListElement, createShowMoreButtonElement(), RenderPosition.BEFOREEND);
+renderTemplate(filmsListContainerElement, createShowMoreButtonElement(), RenderPosition.AFTEREND);
 
 renderTemplate(footerElement, createFooterStatisticsElement(), RenderPosition.BEFOREEND);
+
+renderTemplate(footerElement, createFilmDetailsElement(), RenderPosition.AFTEREND);
+
+const filmDetailsElement = document.querySelector('.film-details');
+filmDetailsElement.style.display = 'none';
