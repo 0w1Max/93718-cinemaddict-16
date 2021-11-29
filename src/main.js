5c74c1fc301a06b18099cb68c1257ab7ec384014
Макс <0w1.go.0w1@gmail.com>
2021-11-29 14:07:35 +0300

    Написал функцию для отрисовки элементов на сайте. Объявил переменные, отвечающие за место отрисовки в вёрстке

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import {RenderPosition, renderTemplate} from './render.js';
+
+const headerElement = document.querySelector('.header');
+const mainElement = document.querySelector('.main');
+const footerElement = document.querySelector('.footer');
