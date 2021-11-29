dd73abc26b3676e4208cff1dc79aa377fd437add
Макс <0w1.go.0w1@gmail.com>
2021-11-29 14:43:45 +0300

    Создал компонент, отвечающий за карточки фильма и отрисовал его на сайте

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import {RenderPosition, renderTemplate} from './render.js';
 import {createProfileElement} from './view/profile-view.js';
 import {createNavElement, createSortElement} from './view/menu-view.js';
+import {createFilmsElement} from './view/films-view.js';
 
 const headerElement = document.querySelector('.header');
 const mainElement = document.querySelector('.main');
@@ -9,3 +10,4 @@ const footerElement = document.querySelector('.footer');
 renderTemplate(headerElement, createProfileElement(), RenderPosition.BEFOREEND);
 renderTemplate(mainElement, createNavElement(), RenderPosition.BEFOREEND);
 renderTemplate(mainElement, createSortElement(), RenderPosition.BEFOREEND);
+renderTemplate(mainElement, createFilmsElement(), RenderPosition.BEFOREEND);
