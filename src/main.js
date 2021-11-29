cdf455cca82edac41368ed1e90d5cd8cb47673ac
Макс <0w1.go.0w1@gmail.com>
2021-11-29 17:46:39 +0300

    Рефакторинг. Создал два компонента для отрисовки карточки фильма

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import {RenderPosition, renderTemplate} from './render.js';
 import {createProfileElement} from './view/profile-view.js';
 import {createNavElement, createSortElement} from './view/menu-view.js';
-import {createFilmsElement} from './view/films-view.js';
+import {createFilmsListElement} from './view/films-list-view.js';
+import {createFilmCardElement} from './view/film-card-view.js';
 
 const headerElement = document.querySelector('.header');
 const mainElement = document.querySelector('.main');
@@ -10,4 +11,9 @@ const footerElement = document.querySelector('.footer');
 renderTemplate(headerElement, createProfileElement(), RenderPosition.BEFOREEND);
 renderTemplate(mainElement, createNavElement(), RenderPosition.BEFOREEND);
 renderTemplate(mainElement, createSortElement(), RenderPosition.BEFOREEND);
-renderTemplate(mainElement, createFilmsElement(), RenderPosition.BEFOREEND);
+
+renderTemplate(mainElement, createFilmsListElement(), RenderPosition.BEFOREEND);
+const filmsListElement = document.querySelector('.films-list__container');
+for (let index = 0; index < 5; index++) {
+  renderTemplate(filmsListElement, createFilmCardElement(), RenderPosition.BEFOREEND);
+}
