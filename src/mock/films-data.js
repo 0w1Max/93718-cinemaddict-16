725b7379dbc4235735a4900e9271057632de399d
Макс <0w1.go.0w1@gmail.com>
2021-12-02 13:10:24 +0300

    Написал функцию для получения данных случайного названия и постера фильма

--- src/mock/films-data.js
+++ src/mock/films-data.js
@@ -6,3 +6,41 @@ const getRandomInteger = (a = 0, b = 1) => {
 
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
+
+const POSTERS_DIR = '/images/posters/';
+
+const generateFilmTitleAndPoster = () => {
+  const filmsTitlesAndPosters = [
+    {
+      'title': 'The Dance of Life',
+      'poster': 'the-dance-of-life.jpg'
+    },
+    {
+      'title': 'Sagebrush Trail',
+      'poster': 'sagebrush-trail.jpg'
+    },
+    {
+      'title': 'The Man with the Golden Arm',
+      'poster': 'the-man-with-the-golden-arm.jpg'
+    },
+    {
+      'title': 'Santa Claus Conquers the Martians',
+      'poster': 'santa-claus-conquers-the-martians.jpg'
+    },
+    {
+      'title': 'Popeye the Sailor Meets Sindbad the Sailor',
+      'poster': 'popeye-meets-sinbad.png'
+    },
+    {
+      'title': 'The Great Flamarion',
+      'poster': 'the-great-flamarion.jpg'
+    },
+    {
+      'title': 'Made for Each Other',
+      'poster': 'made-for-each-other.png'
+    }
+  ];
+
+  const randomIndex = getRandomInteger(0, filmsTitlesAndPosters.length - 1);
+  return filmsTitlesAndPosters[randomIndex];
+};
