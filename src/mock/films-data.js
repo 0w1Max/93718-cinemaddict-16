663d9c358c92e5fea268bd21d90261ce27d58474
Макс <0w1.go.0w1@gmail.com>
2021-12-02 13:22:37 +0300

    Добавил данные для генерации случайного описания фильма

--- src/mock/films-data.js
+++ src/mock/films-data.js
@@ -45,12 +45,32 @@ const generateFilmTitleAndPoster = () => {
   return filmsTitlesAndPosters[randomIndex];
 };
 
+const generateDescription = () => {
+  const filmDescriptions = [
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+    'Cras aliquet varius magna, non porta ligula feugiat eget. ',
+    'Fusce tristique felis at fermentum pharetra. ',
+    'Aliquam id orci ut lectus varius viverra. ',
+    'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. ',
+    'Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. ',
+    'Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. ',
+    'Sed sed nisi sed augue convallis suscipit in sed felis. ',
+    'Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. ',
+    'In rutrum ac purus sit amet tempus.'
+  ];
+
+  const randomIndex = getRandomInteger(0, filmDescriptions.length - 1);
+
+  return filmDescriptions.filter((description, index) => index <= randomIndex).join('');
+};
+
 const generateFilm = () => {
   const newFilmTitleAndPoster = generateFilmTitleAndPoster();
 
   return {
     title: newFilmTitleAndPoster.title,
-    poster: `${POSTERS_DIR}${newFilmTitleAndPoster.poster}`
+    poster: `${POSTERS_DIR}${newFilmTitleAndPoster.poster}`,
+    description: generateDescription()
   };
 };
 
